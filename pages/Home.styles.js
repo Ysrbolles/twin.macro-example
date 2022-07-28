6538caa669b496a154feee3dc9b9cf6def008405
Yassir BOLLES <ysrbolles@gmail.com>
2022-07-28 10:17:05 +0100

    🍱 setup twin.macro example

---
+++ pages/Home.styles.js
@@ -0,0 +1,3 @@
+import tw from "twin.macro";
+
+export const TestStyles = tw.div`text-3xl text-primary`;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import "twin.macro";
+import { TestStyles } from "./Home.styles";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center ">
+      <h1 className="text-3xl text-primary">Hello world!</h1>
+      <h1 tw="text-3xl text-primary">Hello twin!</h1>
+      <TestStyles>Hello Styled Component</TestStyles>
+    </div>
+  );
+}
